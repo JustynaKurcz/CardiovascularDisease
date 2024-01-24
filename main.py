3da0007abf8a4d4f9ccde0e7299e1e9e6f35fca1
Justyna <justyna.kurcz1@gmail.com>
2024-01-24 21:48:14 +0100

    update

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ model = RandomForestClassifier()
 
 # Zdefiniuj przestrzeń hiperparametrów do przeszukania
 param_dist = {
-    'n_estimators': [100, 150, 200, 250],
+    'n_estimators': [50, 100, 150, 200],
     'max_depth': [None, 10, 20, 30],
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 4],
